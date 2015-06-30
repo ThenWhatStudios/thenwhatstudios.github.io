921bad88e1f191cc6a763f55e009199f014907ff
Ghifari160 <ghifari160@gmail.com>
2015-06-30 15:38:25 -0500

    Added templates

---
+++ templates/template.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Then What Studios</title>
+<meta name="copyright" content="Copyright © Then What Studios">
+<!-- G+ Direct Connect -->
+<link rel="publisher" href="https://plus.google.com/108518772326054557103">
+<!-- Organization Metadata -->
+<script type="application/ld+json">
+{
+	"@context"		: "http://schema.org",
+	"@type"			: "Organization",
+	"url"			: "http://thenwhatstudios.github.io",
+	"logo"			: "http://thenwhatstudios.github.io/asset/icons/logo.png",
+	"contactPoint"	: [
+		{
+			"@type"	: "ContactPoint",
+			"url"	: "http://thenwhatstudios.github.io",
+			"email"	: "thenwhatstudios@gmail.com",
+			"contactType" : "customer service"
+		} ]
+}
+</script>
+<!-- Site Metadata -->
+<script type="application/ld+json">
+{
+	"@context"		: "http://schema.org",
+	"@type"			: "WebSite",
+	"name"			: "Then What Studios",
+	"alternateName"	: "TWStudios Official Website",
+	"url"			: "http://thenwhatstudios.github.io",
+	"sameAs"		: [ "https://www.facebook.com/ThenWhatStudios",
+						"https://twitter.com/thenwhatstudios",
+						"https://plus.google.com/108518772326054557103" ]
+}
+</script>
+<link rel="apple-touch-icon" sizes="57x57" href="http://thenwhatstudios.github.io/asset/icons/apple57.png">
+<link rel="apple-touch-icon" sizes="114x114" href="http://thenwhatstudios.github.io/asset/icons/apple114.png">
+<link rel="apple-touch-icon" sizes="72x72" href="http://thenwhatstudios.github.io/asset/icons/apple72.png">
+<link rel="apple-touch-icon" sizes="60x60" href="http://thenwhatstudios.github.io/asset/icons/apple60.png">
+<link rel="apple-touch-icon" sizes="120x120" href="http://thenwhatstudios.github.io/asset/icons/apple120.png">
+<link rel="apple-touch-icon" sizes="76x76" href="http://thenwhatstudios.github.io/asset/icons/apple76.png">
+<link rel="apple-touch-icon" sizes="152x152" href="http://thenwhatstudios.github.io/asset/icons/apple152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="http://thenwhatstudios.github.io/asset/icons/apple180.png">
+<link rel="icon" type="image/png" href="http://thenwhatstudios.github.io/asset/icons/icon192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="http://thenwhatstudios.github.io/asset/icons/icon160.png" sizes="160x160">
+<link rel="icon" type="image/png" href="http://thenwhatstudios.github.io/asset/icons/icon96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="http://thenwhatstudios.github.io/asset/icons/icon16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="http://thenwhatstudios.github.io/asset/icons/icon32.png" sizes="32x32">
+<meta name="msapplication-TileColor" content="#2d89ef">
+<meta name="msapplication-TileImage" content="http://thenwhatstudios.github.io/asset/icons/ms144.png">
+<meta name="apple-mobile-web-app-title" content="Then What Studios">
+<meta name="application-name" content="Then What Studios">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href='http://fonts.googleapis.com/css?family=Righteous' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="/asset/css/v2-0/v2.0.css">
+<link rel="stylesheet" href="/asset/css/v2-0/v2.0-mdeia.css">
+<script src="https://apis.google.com/js/platform.js" defer></script>
+<script id="twitter-wjs" src="http://platform.twitter.com/widgets.js" defer></script>
+<script id="facebook-jssdk" src="//connect.facebook.net/en_US/sdk.js#xfbml=1&amp;version=v2.3&amp;appId=268998356452841" defer></script>
+<script src="/asset/js/jquery.js" defer></script>
+<script src="/asset/js/v2.0.js" defer></script>
+</head>
+
+<body>
+<div id="loading">
+	<div class="holder">Loading...</div>
+	<div class="left"></div>
+	<div class="right"></div>
+</div>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=268998356452841";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<header class="header">
+	<div class="header-logo"></div>
+	<div class="header-company">
+		<div class="header-company-title">Then What Studios</div>
+		<div class="header-company-tagline">Professionalism and laziness combined</div>
+	</div>
+</header>
+
+<nav class="menu">
+	<a href="#" class="menu-mobile-open">
+		<div class="menu-mobile"></div>
+	</a>
+	<div class="menu-title hidden">
+		<a href="#"></a>
+	</div>
+	<nav class="menu-nav">
+		<ul>
+			<li>
+				<a href="/products">Products</a>
+				<ul>
+					<li><a href="/resources/project/fortune">Project: Fortune</a></li>
+					<li><a href="/resources/project/red">Project: Red</a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="/games">Games</a>
+				<ul>
+					<li><a href="/resources/codename/dust">Codename: Dust</a></li>
+					<li><a href="/resources/codename/eucalyptus">Codename: Eucalyptus</a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="/about">About</a>
+				<ul>
+					<li><a href="/about/company">Company</a></li>
+					<li><a href="/about/press">Press</a></li>
+					<li><a href="//twstudiosblog.wordpress.com/">Newsroom</a></li>
+					<li><a href="/resources/legal">Legal</a></li>
+					<li><a href="/about/staff">Staff Directory</a></li>
+				</ul>
+			</li>
+			<li><a href="/about/contact">Contact</a></li>
+		</ul>
+	</nav>
+</nav>
+
+<header class="featured">
+	<div class="parent" data-background="/asset/icons/codename-dust.png" data-order="1">
+		<div class="title">Featured Project: "Codename: Dust"</div>
+		<div class="description">
+			Codename: Dust is a Sci-fi game that took place 50 years after the extinction of fuel.
+		</div>
+		<div class="social">
+			<div class="doc"><a href="/resources/codename/dust/design-doc.html"><div>Design Document</div></a></div>
+			<div class="wdir"><a href="/rdr/#!/codename_dust_workdir"><div>Working Directory</div></a></div>
+		</div>
+	</div>
+	<div class="parent" data-background="/asset/icons/codename-dust.png" data-order="2">
+		<div class="title">W.I.P. Project: "Codename: Eucalyptus"</div>
+		<div class="description">
+			Codename: Eucalyptus is a murder mystery game. It is currently under designing state.
+		</div>
+		<div class="social">
+			<div class="wdir"><a href="/rdr/#!/codename_eucalyptus_workdir"><div>Working Directory</div></a></div>
+		</div>
+	</div>
+	<div class="parent" data-order="3">
+		<div class="title">Classified Project: "Project: Fortune"</div>
+		<div class="description">
+			Project: Fortune is currently classified.
+		</div>
+		<div class="social">
+			<div class="doc"><a href="/resources/project/fortune/proposal.html"><div>Proposal</div></a></div>
+			<div class="wdir"><a href="/rdr/#!/project_fortune_workdir"><div>Working Directory</div></a></div>
+		</div>
+	</div>
+</header>
+
+<div class="wrapper">
+	<div class="content">
+		aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+	</div>
+	
+	<nav class="sidebar">
+		<div class="gp">
+			<div class="g-page" data-href="//plus.google.com/u/0/108518772326054557103" data-rel="publisher"></div>
+		</div>
+		<div class="fb">
+			<div class="fb-page" data-href="https://www.facebook.com/ThenWhatStudios" data-width="300" data-hide-cover="false" data-show-facepile="false" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/ThenWhatStudios"><a href="https://www.facebook.com/ThenWhatStudios">Then What Studios</a></blockquote></div></div>
+		</div>
+		<div class="t">
+			<a class="twitter-timeline" width="300" height="500" href="https://twitter.com/ThenWhatStudios/lists/then-what-studios" data-chrome="nofooter transparent" data-theme="light" data-link-color="#427fed" data-widget-id="599725333286387712">Tweets from Then What Studios staff</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</div>
+	</nav>
+</div>
+
+<div class="container">
+	<footer class="footer">
+		<div class="footer-social">
+			<a href="https://www.facebook.com/ThenWhatStudios" target="_blank">
+				<div class="fb"></div>
+			</a>
+			<a href="https://twitter.com/thenwhatstudios" target="_blank">
+				<div class="t"></div>
+			</a>
+			<a href="https://plus.google.com/108518772326054557103" target="_blank">
+				<div class="gp"></div>
+			</a>
+			<a href="https://www.youtube.com/channel/UCWNSBy7bwvjzQfXHmx5bWzg/" target="_blank">
+				<div class="yt"></div>
+			</a>
+			<a href="https://github.com/ThenWhatStudios" target="_blank">
+				<div class="gh"></div>
+			</a>
+		</div>
+		
+		<a href="/" class="footer-logo-link">
+			<span class="footer-logo"></span>
+		</a>
+		
+		<nav class="footer-nav">
+			<ul>
+				<li>Copyright &copy; 2015 Then What Studios</li>
+				<li><a href="//twstudiosblog.wordpress.com/">Newsroom</a></li>
+				<li><a href="/resources/legal">Legal</a></li>
+				<li><a href="/about">About</a></li>
+			</ul>
+		</nav>
+	</footer>
+</div>
+</body>
+</html>
